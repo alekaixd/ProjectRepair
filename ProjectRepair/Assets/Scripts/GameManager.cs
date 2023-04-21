89eb30a3159d0fa244c328a88461c8786a335c2f
alekaixd <aleksi.pirttinen@gmail.com>
2023-04-21 11:50:14 +0300

    heitän

--- ProjectRepair/Assets/Scripts/GameManager.cs
+++ ProjectRepair/Assets/Scripts/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]public GameObject waterLevel;
+    private Transform waterTransform;
+    public float waterToAdd = 0;
+    private float waterAdded = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waterTransform = waterLevel.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (waterAdded <= waterToAdd)
+        {
+            waterTransform.position = new Vector2(waterTransform.position.x, waterTransform.position.y + 0.01f);
+            waterAdded += 0.01f;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            waterToAdd += 1.0f;
+        }
     }
 }
